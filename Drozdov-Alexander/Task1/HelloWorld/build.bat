53e3620735ceb823050820255439e04d6f9488c2
AlexandrosDaSilva <sanek-97@yandex.ru>
2016-11-29 19:35:20 +0300

    Task 1

--- Drozdov-Alexander/Task1/HelloWorld/build.bat
+++
@@ -1,6 +0,0 @@
-md build\classes
-javac -d build\classes src\basic\HelloWorld.java
-
-echo Main-Class: basic.HelloWorld>manifest
-md build\jar
-jar cfm build\jar\helloworld.jar manifest -C build\classes .
