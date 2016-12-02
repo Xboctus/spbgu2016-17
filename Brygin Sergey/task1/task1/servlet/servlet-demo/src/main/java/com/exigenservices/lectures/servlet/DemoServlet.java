af90b5b769f3de7022abc1046ebcdfa5f76bebdf
Sergey <sergeybrygen@mail.ru>
2016-12-02 14:51:50 +0300

    Add files via upload

---
+++ Brygin Sergey/task1/task1/servlet/servlet-demo/src/main/java/com/exigenservices/lectures/servlet/DemoServlet.java
@@ -0,0 +1,19 @@
+package com.exigenservices.lectures.servlet;
+
+import com.sun.net.httpserver.HttpServer;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by sergey on 30.11.2016.
+ */
+public class DemoServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+}
