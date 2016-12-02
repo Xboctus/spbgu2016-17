df59bd6122aa04aaeb73e29551d3cd99b2f8adc2
fogelmariya <fogelmariya@gmail.com>
2016-12-02 15:49:30 +0300

    Add files via upload

---
+++ Task4/user_manager/user-manager-ejb/src/main/java/com/exigenservices/lectures/UserManagerSessionBean.java
@@ -0,0 +1,29 @@
+package com.exigenservices.lectures;
+
+import javax.ejb.Remote;
+import javax.ejb.Stateless;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * UserManager bean implementation
+ *
+ * Created by Маруся on 02.12.2016.
+ */
+@Remote(UserManager.class)
+@Stateless(mappedName = "UserManagerSessionBean")
+public class UserManagerSessionBean implements UserManager {
+    @Override
+    public Map<Character, Integer> countLetterAmounts(String text) {
+        HashMap<Character, Integer> queryResult = new HashMap<Character, Integer>();
+        for(int i = 0; i < text.length(); i++) {
+            Integer charOccurences = queryResult.get(text.charAt(i));
+            if(charOccurences == null) {
+                queryResult.put(text.charAt(i), 1);
+            } else {
+                queryResult.put(text.charAt(i), charOccurences + 1);
+            }
+        }
+        return queryResult;
+    }
+}
---
+++ Task4/user_manager/user-manager-webapp/src/main/webapp/index.jsp
@@ -0,0 +1,23 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+
+<html>
+<head>
+    <title>Users management</title>
+    <link rel="stylesheet" type="text/css" href="${pageContext.request.contextPath}/css/common.css">
+</head>
+
+<body>
+
+<div id="maindiv">
+    <h2>
+        Enter your text!
+    </h2>
+    <textarea form="mainform" name="texttoparse"  rows="15" cols="50"></textarea>
+    <form id="mainform" action="${pageContext.request.contextPath}/submit" method="post">
+        <input type="submit" value="OK"/>
+    </form>
+</div>
+
+</body>
+</html>
